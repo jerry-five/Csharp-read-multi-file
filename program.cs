9e64794fe92ff0098ca1e9d0759d02f5b6e16c67
Tai Pham <36804773+pktai@users.noreply.github.com>
2019-09-09 21:31:58 +0700

    Update program.cs

--- program.cs
+++ program.cs
@@ -4,16 +4,15 @@ using System.IO;
 using System.Diagnostics;
 using System.Linq;
 using System.Collections;
-using Bytescout.Spreadsheet;
 using System.Text.RegularExpressions;
-
+using Bytescout.Spreadsheet;
 namespace ConsoleApp3
 {
     class Program
     {
-        private static string[][] ReadAllFilesInDirectory(string topLevelDirectory, string[][] arr)
+        private static string[] ReadAllFilesInDirectory(string topLevelDirectory, string[] arr)
         {
-            string[][] arrFunc = arr;
+            string[] arrFunc = arr;
             const string searchPattern = "*.xml";
             const string searchPattern1 = "*.asp";
             var subDirectories = Directory.EnumerateDirectories(topLevelDirectory);
@@ -22,13 +21,13 @@ namespace ConsoleApp3
             var filesInDirectory = filesInDirectory1.Concat(filesInDirectory2).ToArray();
             foreach (var subDirectory in subDirectories)
             {
-                arrFunc = arrFunc.Concat(ReadAllFilesInDirectory(subDirectory, arrFunc)).ToArray();//recursion
+                arrFunc = arrFunc.Concat(ReadAllFilesInDirectory(subDirectory, arrFunc)).Distinct().ToArray();//recursion
             }
-            arrFunc = arrFunc.Concat(IterateFiles(filesInDirectory, topLevelDirectory)).ToArray();
+
+            arrFunc = arrFunc.Concat(filesInDirectory).ToArray();
             return arrFunc;
         }
-
-        private static string[][] IterateFiles(IEnumerable<string> files, string directory)
+        private static string[][] IterateFiles(IEnumerable<string> files)
         {
             string[][] termsList = new string[0][];
 
@@ -39,11 +38,12 @@ namespace ConsoleApp3
                     string[] lines = File.ReadAllLines(file);
                     foreach (var line in lines)
                     {
-                        if(!String.IsNullOrEmpty(line.Trim()))
+                        //handle check line on string;
+                        if (!String.IsNullOrEmpty(line.Trim()))
                         {
 
                             String output = Regex.Replace(line, @"\>([^\[\]]+)\<", "");
-                            if(output != null)
+                            if (output != null)
                             {
                                 string[] fileExtention = { file, output };
                                 termsList = termsList.Concat(new string[][] { fileExtention }).ToArray();
@@ -67,15 +67,17 @@ namespace ConsoleApp3
             Sheet.Columns[0].Width = 250;
             Sheet.Cell("B1").Value = "English";
             Sheet.Columns[1].Width = 250;
-            string[][] termsList = new string[0][];
-            string[][] tes = ReadAllFilesInDirectory(@"C:\Users\ADMIN\Desktop\Readfile\test", termsList);
-            for (int i = 2; i < tes.Length; i++)
+            string[] arr = new string[0];
+            string[] tes = ReadAllFilesInDirectory(@"C:\Users\Donald-Trump\Desktop\Readfile", arr);
+            string[][] readFile = new string[0][];
+            readFile = IterateFiles(tes);
+            for (int i = 2; i < readFile.Length; i++)
             {
-                Sheet.Cell(Convert.ToString("A" + i)).Value = tes[i][0];
-                Sheet.Cell(Convert.ToString("B" + i)).Value = tes[i][1];
+
+                Sheet.Cell(Convert.ToString("A" + i)).Value = readFile[i][0];
+                Sheet.Cell(Convert.ToString("B" + i)).Value = readFile[i][1];
             }
             document.SaveAs("Output.xls");
-
             document.Close();
             Process.Start("Output.xls");
         }
